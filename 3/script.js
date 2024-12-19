f7709c71b68e22f3a3d3ab53ae7d9e498ff284ca
fthuggy <179480003+fthuggy@users.noreply.github.com>
2024-12-19 16:30:47 +0100

    testing out styling with js

---
+++ 3/script.js
@@ -0,0 +1,26 @@
+const form = document.querySelector("form");
+
+form.style.display = "flex";
+form.style.flexDirection = "column";
+form.style.flexWrap = "no-wrap";
+form.style.justifyContent = "center";
+form.style.alignItems = "center";
+form.style.alignContent = "space-between";
+form.style.backgroundColor = "grey";
+form.style.color = "white";
+form.style.fontFamily = "verdana";
+form.style.fontSize = "16px";
+form.style.width = "600px";
+form.style.margin = "0 auto";
+form.style.padding = "50px";
+form.style.borderRadius = "8px";
+form.style.boxShadow = "0 8px 15px rgba(0, 0, 0, 0.3)";
+
+/*const writing = document.querySelector("input");
+
+writing.style.marginBottom = "";*/
+
+const button = document.querySelector("input[type=submit]");
+
+button.style.backgroundColor = "blue";
+button.style.color = "white";
