e60fe4fd4a2888cc306718243936927c86db5dbe
fthuggy <179480003+fthuggy@users.noreply.github.com>
2024-12-24 16:29:09 +0100

    more styling and added a div element for the  selection part

--- 3/script.js
+++ 3/script.js
@@ -1,26 +1,41 @@
 const form = document.querySelector("form");
 
-form.style.display = "flex";
-form.style.flexDirection = "column";
-form.style.flexWrap = "no-wrap";
-form.style.justifyContent = "center";
-form.style.alignItems = "center";
-form.style.alignContent = "space-between";
-form.style.backgroundColor = "grey";
-form.style.color = "white";
-form.style.fontFamily = "verdana";
-form.style.fontSize = "16px";
-form.style.width = "600px";
-form.style.margin = "0 auto";
-form.style.padding = "50px";
-form.style.borderRadius = "8px";
-form.style.boxShadow = "0 8px 15px rgba(0, 0, 0, 0.3)";
+if (form) {
+  form.style.display = "flex";
+  form.style.flexDirection = "row";
+  form.style.flexWrap = "wrap";
+  form.style.justifyContent = "space-between";
+  form.style.alignItems = "center";
+  form.style.backgroundColor = "#e2e8f0";
+  form.style.color = "black";
+  form.style.fontFamily = "Verdana";
+  form.style.fontSize = "18px";
+  form.style.width = "50%";
+  form.style.maxWidth = "700px";
+  form.style.maxHeight = "900px";
+  form.style.margin = "0 auto";
+  form.style.padding = "50px";
+  form.style.borderRadius = "8px";
+  form.style.boxShadow = "0 8px 15px rgba(0, 0, 0, 0.3)";
+  form.style.marginTop = "50px";
+}
 
-/*const writing = document.querySelector("input");
-
-writing.style.marginBottom = "";*/
+const message = document.getElementById("message");
+if (message) {
+  message.style.width = "200px";
+  message.style.padding = "10px";
+  message.style.marginTop = "10px";
+  message.style.marginLeft = "60px";
+}
 
 const button = document.querySelector("input[type=submit]");
 
-button.style.backgroundColor = "blue";
-button.style.color = "white";
+if (button) {
+  button.style.backgroundColor = "#60a5fa";
+  button.style.color = "Black";
+  button.style.marginTop = "40px";
+  button.style.borderRadius = "10px";
+  button.style.padding = "10px";
+  button.style.width = "200px";
+  button.style.fontSize = "18px";
+}
