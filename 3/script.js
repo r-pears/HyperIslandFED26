41eb8f6569f6b3624c202382a3dbcd3fda6b5858
minzikimi <minji.kim@hyperisland.se>
2025-01-06 09:42:58 +0100

    assignemnt 3 done

--- 3/script.js
+++ 3/script.js
@@ -8,16 +8,41 @@ document.addEventListener("DOMContentLoaded", function(){
     alert("form submitted!");
   });
 
-  document.querySelector('label[for="name"]').textContent = "Your name : ";
-  document.querySelector('label[for="email"]').textContent = "Your email : ";
-  document.querySelector('label[for="phone"]').textContent = "Phone number : ";
-  document.querySelector('label[for="message"]').textContent = "Messages : ";
-  document.querySelector('label[for="options"]').textContent = "Select your option : ";
+  const nameInput = document.querySelector('label[for="name"]');
+  const emailInput = document.querySelector('label[for="email"]');
+  const phoneInput = document.querySelector('label[for="phone"]');
+  const messageInput = document.querySelector('label[for="message"]');
+  const optionsTitle = document.querySelector('label[for="options"]');
+  const options = document.querySelector("#options");
+  const inputs = document.querySelectorAll("input");
+  const radioLabels = document.querySelectorAll('label[for^="radio"]');
+  const textArea = document.querySelector("textarea");
+
+
+  nameInput.textContent = "Your name : ";
+  emailInput.textContent = "Your email : ";
+  phoneInput.textContent = "Phone number : ";
+  messageInput.textContent = "Messages : ";
+  optionsTitle.textContent = "Select your option : ";
 
   form.style.backgroundColor = "#f0f0f0";
   form.style.textAlign = "center";
   form.style.fontSize = "2rem";
   submitBtn.style.backgroundColor = "blue";
-  submitBtn.style.color ="white";
-  submitBtn.style.fontSize ="1.5rem";
+  submitBtn.style.color = "white";
+  submitBtn.style.fontSize = "1.5rem";
+
+  textArea.style.color = "blue";
+  textArea.style.backgroundColor = "beige";
+
+  options.style.backgroundColor = "beige";
+  options.style.fontSize = "1.5rem"
+
+  inputs.forEach(input => {
+    input.style.height = "3rem";
+  });
+
+  radioLabels.forEach((label, index) => {
+    label.textContent = `Choice ${index + 1}`;
+  });
 });
