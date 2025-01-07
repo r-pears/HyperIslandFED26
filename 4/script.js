def7886246ce08a0a147f53f74af90a23548ce2e
Dylano <167641317+dstjp@users.noreply.github.com>
2025-01-07 14:33:52 +0100

    a4 done

--- 4/script.js
+++ 4/script.js
@@ -6,5 +6,31 @@ buttonContainer.addEventListener("click", (event) => {
 });
 
 buttonChildContainer.addEventListener("click", (event) => {
+  event.stopPropagation();
+  if(event.target.id === "button4") {
+    alert(event.target.textContent);
+    return;
+  } else if (event.target.id === "button5") {
+    alert(event.target.textContent);
+    return;
+  }
   alert("Child container clicked!");
 });
+
+const button2 = document.getElementById("button2")
+
+button2.onclick = function(event) {
+  event.stopPropagation();
+  alert("button2");
+}
+
+const button3 = document.getElementById("button3");
+
+button3.addEventListener("click", (event) => {
+event.stopPropagation();
+  alert("button3");
+});
+
+
+
+
