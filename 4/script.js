1bc5f159f3a8e64c8528d98fac97a70737a6ac63
Xutik <kseniia.prakhova@hyperisland.se>
2025-01-07 22:29:48 +0100

    btns

--- 4/script.js
+++ 4/script.js
@@ -7,4 +7,21 @@ buttonContainer.addEventListener("click", (event) => {
 
 buttonChildContainer.addEventListener("click", (event) => {
   alert("Child container clicked!");
+  if (event.target.tagName === 'BUTTON') {
+    alert(`${event.target.textContent} CLICKED`);
+  }
 });
+
+const btn2 = document.getElementById("button2")
+btn2.onclick = () => {
+  alert('BTN2 clicked')
+}
+
+const btn3 = document.getElementById("button3")
+btn3.addEventListener('click', (e) => {
+  e.stopPropagation()
+  alert('BTN3 clicked')
+})
+
+
+
