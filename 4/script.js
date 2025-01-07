8902dc12c904e35103faa62f26938311501c6325
ghazal lalehgani <lalehganighazal@gmail.com>
2025-01-07 16:14:26 +0100

    javascript update

--- 4/script.js
+++ 4/script.js
@@ -1,10 +1,18 @@
-const buttonContainer = document.getElementById("buttonContainer");
 const buttonChildContainer = document.getElementById("buttonChildContainer");
-
-buttonContainer.addEventListener("click", (event) => {
-  alert("Container clicked!");
+const button2 = document.getElementById("button2");
+const button3 = document.getElementById("button3");
+const button4 = document.getElementById("button4");
+const button5 = document.getElementById("button5");
+button2.onclick = function () {
+  alert("Button 2 clicked!");
+};
+button3.addEventListener("click", () => {
+  alert("Button 3 clicked!");
 });
-
-buttonChildContainer.addEventListener("click", (event) => {
-  alert("Child container clicked!");
+buttonChildContainer.addEventListener("click", (e) => {
+  if (e.target.id === "button4") {
+    alert("Button 4 clicked!");
+  } else if (e.target.id === "button5") {
+    alert("Button 5 clicked!");
+  }
 });
