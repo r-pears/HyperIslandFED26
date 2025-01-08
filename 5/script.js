3716cfaceac6348baeada39dedf142fe5fb3df6a
Mohamed Guled <mohamed.guled@hyperisland.se>
2025-01-08 22:36:14 +0100

    Assignment 5

--- 5/script.js
+++ 5/script.js
@@ -6,13 +6,16 @@ const apiUrl = "https://api.example.com/data";
 function fetchData(url) {
   fetch(url)
     .then((response) => {
-      if (response.ok) {
+      if (!response.ok) {
         throw new Error("Network response was not ok " + response.statusText);
       }
       return response.json();
     })
     .then((data) => {
       console.log(data);
+    })
+    .catch((error) => {
+      console.error("There was problem with the fetch operation:", error);
     });
 }
 
@@ -20,7 +23,27 @@ fetchData(apiUrl);
 
 // Function to make a POST request
 // TODO: Implement the function
-function postData(url, data) {}
+function postData(url, data) {
+  fetch(url, {
+    method: "POST", 
+    headers: {
+      "Content-Type": "application/json", 
+    },
+    body: JSON.stringify(data), 
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Could not post data: " + response.statusText);
+      }
+      return response.json();
+    })
+    .then((responseData) => {
+      console.log("Data sucessfully posted!!:", responseData);
+    })
+    .catch((error) => {
+      console.error("Eror posting data:", error);
+    });
+}
 
 const dataToPost = {
   name: "John Doe",
