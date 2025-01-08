98286d534fb2e5f17a203711b4505f631451f990
Alexandra <alexandra.riva@hyperisland.se>
2025-01-08 12:26:25 +0100

    Assignment 4 done

--- 4/script.js
+++ 4/script.js
@@ -1,10 +1,23 @@
 const buttonContainer = document.getElementById("buttonContainer");
 const buttonChildContainer = document.getElementById("buttonChildContainer");
 
+// Parent event listener for button container
 buttonContainer.addEventListener("click", (event) => {
   alert("Container clicked!");
 });
 
+//DOM Level 0
+const button2 = document.getElementById("button2");
+button2.onclick = function () {alert("Button2 is clicked!");};
+
+//DOM Level 2
+const button3 = document.getElementById("button3");
+button3.addEventListener("click", function (event) {alert("Button3 is clicked!");});
+
+//Event Delegation for Button 4 and 5
 buttonChildContainer.addEventListener("click", (event) => {
-  alert("Child container clicked!");
+  if (event.target.tagName === "BUTTON") {
+    alert(`${event.target.textContent} is clicked!`);
+    event.stopPropagation();
+  }
 });
