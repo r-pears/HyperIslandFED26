8a86caeeb027566bf3ee7115f4c19c0926609879
Alexandra <alexandra.riva@hyperisland.se>
2025-01-08 22:46:49 +0100

    Completed assignment_5

--- 5/script.js
+++ 5/script.js
@@ -6,21 +6,43 @@ const apiUrl = "https://api.example.com/data";
 function fetchData(url) {
   fetch(url)
     .then((response) => {
-      if (response.ok) {
+      if (!response.ok) {
         throw new Error("Network response was not ok " + response.statusText);
       }
       return response.json();
     })
     .then((data) => {
-      console.log(data);
+      if (data) {
+        console.log(data); 
+      }
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
     });
-}
+  }
 
 fetchData(apiUrl);
 
 // Function to make a POST request
 // TODO: Implement the function
-function postData(url, data) {}
+function postData(url, data) {
+  fetch(url, {
+    method: "POST", 
+    headers: {
+      "Content-Type": "application/json", 
+    },
+    body: JSON.stringify(data),
+  })
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Error: " + response.statusText); 
+    }
+    return response.json(); 
+  })
+  .then((data) => console.log("Data posted successfully:", data)) 
+  .catch((error) => console.error("Error posting data:", error)); 
+}
+
 
 const dataToPost = {
   name: "John Doe",
