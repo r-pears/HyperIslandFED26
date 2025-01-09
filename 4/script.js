401f30a29dbd442a2f49040c38f0da3f452376a0
odai-dh <odai.dahi@hyperisland.se>
2025-01-09 16:21:21 +0100

    a4

--- 4/script.js
+++ 4/script.js
@@ -8,3 +8,31 @@ buttonContainer.addEventListener("click", (event) => {
 buttonChildContainer.addEventListener("click", (event) => {
   alert("Child container clicked!");
 });
+
+//Button 1
+function button1 () {
+  alert("Button 1 clicked!")
+  event.stopPropagation()
+}
+
+//Button 2
+const button2 = document.getElementById("button2")
+button2.addEventListener("click", (event) => {
+  alert("Button 2 clicked!")
+  event.stopPropagation()
+})
+
+//Button 3
+const button3 = document.getElementById("button3")
+button3.addEventListener('click', function(event){
+  alert("Button 3 Clicked!")
+  event.stopPropagation()
+})
+
+//Button 4/5
+buttonChildContainer.addEventListener("click", function(event) {
+  if (event.target.tagName === "BUTTON") {
+    alert(event.target.innerText + " clicked!")
+    event.stopPropagation()
+  }
+})
