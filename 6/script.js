b9e4153b97464c521f499a78c89d3251b37d3367
Mohamed Guled <mohamed.guled@hyperisland.se>
2025-01-12 13:49:36 +0100

    assignment 6 done

--- 6/script.js
+++ 6/script.js
@@ -1,23 +1,41 @@
 // TODO: Set item in localStorage
+localStorage.setItem('key', 'value');
 
 // TODO: Get item from localStorage
+const valueFromLocalStorage = localStorage.getItem('key');
+console.log(valueFromLocalStorage);
 
 // TODO: Remove item from localStorage
+localStorage.removeItem('key');
 
 // TODO: Clear all items from localStorage
+localStorage.clear();
 
 // TODO: Add an object to localStorage
+const myObject = { name: 'Mohamed', age: 27 };
+localStorage.setItem('myObject', JSON.stringify(myObject));
 
 // TODO: Get an object from localStorage
+const objectFromLocalStorage = JSON.parse(localStorage.getItem('myObject'));
+console.log(objectFromLocalStorage);
 
 // TODO: Set item in sessionStorage
+sessionStorage.setItem('key', 'value');
 
 // TODO: Get item from sessionStorage
+const valueFromSessionStorage = sessionStorage.getItem('key');
+console.log(valueFromSessionStorage);
 
 // TODO: Remove item from sessionStorage
+sessionStorage.removeItem('key');
 
 // TODO: Clear all items from sessionStorage
+sessionStorage.clear();
 
 // TODO: Add an object to sessionStorage
+const mySessionObject = { city: 'Stockholm', country: 'Sweden' };
+sessionStorage.setItem('mySessionObject', JSON.stringify(mySessionObject));
 
 // TODO: Get an object from sessionStorage
+const objectFromSessionStorage = JSON.parse(sessionStorage.getItem('mySessionObject'));
+console.log(objectFromSessionStorage);
