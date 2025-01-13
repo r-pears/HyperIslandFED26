6f3b62ab450f89281408a055280e5407b6cf3ce6
hannac7 <yourname@example.com>
2025-01-13 22:45:15 +0100

    finished assignment 6

--- 6/script.js
+++ 6/script.js
@@ -1,23 +1,40 @@
 // TODO: Set item in localStorage
+localStorage.setItem("key", "value");
 
 // TODO: Get item from localStorage
+let getItem = localStorage.getItem("key");
 
 // TODO: Remove item from localStorage
+localStorage.removeItem("key");
 
 // TODO: Clear all items from localStorage
+localStorage.clear();
 
 // TODO: Add an object to localStorage
+let user = {
+    name: "Sigrid", 
+    age: "9",
+};
+
+localStorage.setItem("user", JSON.stringify(user));
 
 // TODO: Get an object from localStorage
+let getUser = JSON.parse(localStorage.getItem(user));
 
 // TODO: Set item in sessionStorage
+sessionStorage.setItem("key", "value");
 
 // TODO: Get item from sessionStorage
+let getSessionStorageItem = sessionStorage.getItem("key");
 
 // TODO: Remove item from sessionStorage
+sessionStorage.removeItem("key");
 
 // TODO: Clear all items from sessionStorage
+sessionStorage.clear();
 
 // TODO: Add an object to sessionStorage
+sessionStorage.setItem("user", JSON.stringify(user));
 
 // TODO: Get an object from sessionStorage
+let sessionStorageUser = JSON.parse(sessionStorage.getItem("user"));
