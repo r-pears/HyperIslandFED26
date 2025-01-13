69234faf03c0ea71da24cbfbbcb6d6e170f68bf4
Anton Hansson <125166515+antxhan@users.noreply.github.com>
2025-01-14 00:22:18 +0100

    assignment 6

--- 6/script.js
+++ 6/script.js
@@ -1,23 +1,35 @@
 // TODO: Set item in localStorage
+localStorage.setItem("key", "value");
 
 // TODO: Get item from localStorage
+localStorage.getItem("key");
 
 // TODO: Remove item from localStorage
+localStorage.removeItem("key");
 
 // TODO: Clear all items from localStorage
+localStorage.clear();
 
 // TODO: Add an object to localStorage
+localStorage.setItem("object", JSON.stringify({ key: "value" }));
 
 // TODO: Get an object from localStorage
+JSON.parse(localStorage.getItem("object"));
 
 // TODO: Set item in sessionStorage
+sessionStorage.setItem("key", "value");
 
 // TODO: Get item from sessionStorage
+sessionStorage.getItem("key");
 
 // TODO: Remove item from sessionStorage
+sessionStorage.removeItem("key");
 
 // TODO: Clear all items from sessionStorage
+sessionStorage.clear();
 
 // TODO: Add an object to sessionStorage
+sessionStorage.setItem("object", JSON.stringify({ key: "value" }));
 
 // TODO: Get an object from sessionStorage
+JSON.parse(sessionStorage.getItem("object"));
