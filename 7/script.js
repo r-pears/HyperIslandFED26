ca9980cc0975ff133e9dd02d3ab7535599a35103
ghazal lalehgani <lalehganighazal@gmail.com>
2025-01-14 15:58:26 +0100

    Assingment 7

--- 7/script.js
+++ 7/script.js
@@ -1,53 +1,33 @@
-/**
- * TODO write a JavaScript function that takes a string and returns it in revserse order.
- * DO NOT use the built-in reverse() method.
- * Should return the reversed string.
- * For example:
- *   reverseString("hello") should return "olleh"
- *   reverseString("world") should return "dlrow"
- *   reverseString("12345") should return "54321"
- */
-function reverseString() {}
+//Solution for Reverse String Function
+function reverseString(str) {
+  let reversed = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
 
-/**
- * TODO write a JavaScript function that takes a number and returns it in the reverse order.
- * Should return the reversed number.
- * For example:
- *   reverseNumber(12345) should return 54321
- *   reverseNumber(98765) should return 56789
- *   reverseNumber(101010) should return 010101
- */
+//Solution for Reverse Number Function
+function reverseNumber(num) {
+  return parseInt(num.toString().split('').reverse().join(''));
+}
 
-function reverseNumber() {}
+//Object Destructuring
+const { name, info: { age } } = person;
 
-/**
- * TODO destruct the following object to get name and age.
- */
-const person = {
-  name: "John",
-  info: {
-    age: 30,
-    city: "New York",
-    job: "Developer",
-  },
-};
+//Array Destructuring
+const [first, second, ...rest] = thisArray;
 
-/**
- * TODO destruct the following array the first and second element, and the remaining elements in a third variable.
- */
+//Palindrome Check Function
+function isPalindrome(str) {
+  // Remove whitespaces and convert to lowercase
+  str = str.replace(/\s/g, '').toLowerCase();
+  
+  // Compare string with its reverse
+  return str === str.split('').reverse().join('');
+}
 
-const thisArray = ["apple", "banana", "cherry", "dates", "elderberry", "fig"];
-
-/**
- * !!!!OPTIONAL!!!!
- * TODO Write a JavaScript function that checks if a string is a palindrome. A palindrome is a word, phrase, number, or other sequence of characters that reads * the same forward and backward. For example, "madam" is a palindrome.
- * The function should return true if the string is a palindrome and false if it is not.
- * For example:
- *    isPalindrome("madam") should return true
- *    isPalindrome("hello") should return false
- *    isPalindrome("12321") should return true
- *    isPalindrome("race car") should return true
- *    isPalindrome("coding") should return false
- */
-
-function isPalindrome() {}
+//Function Test Examples
+console.log(reverseString("hello"));  // Output: "olleh"
+console.log(reverseNumber(12345));    // Output: 54321
+console.log(isPalindrome("madam"));   // Output: true
