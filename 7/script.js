b8be2f0de2cf1888accb016ea31a0617f06ecf94
Asher-zx <zhixuan.wang@hyperisland.se>
2025-01-14 22:40:41 +0100

    assign 7 complete

--- 7/script.js
+++ 7/script.js
@@ -7,7 +7,16 @@
  *   reverseString("world") should return "dlrow"
  *   reverseString("12345") should return "54321"
  */
-function reverseString() {}
+function reverseString(str) {
+  let strIndex = ""
+  for (let i = str.length - 1; i>= 0; i--) {
+    strIndex += str[i]
+  }
+  return strIndex
+}
+
+console.log(reverseString("everthing"));
+
 
 /**
  * TODO write a JavaScript function that takes a number and returns it in the reverse order.
@@ -18,7 +27,19 @@ function reverseString() {}
  *   reverseNumber(101010) should return 010101
  */
 
-function reverseNumber() {}
+function reverseNumber(num) {
+	return (
+    parseFloat(
+      num
+        .toString()
+        .split('')
+        .reverse()
+        .join('')
+    ) * Math.sign(num)
+  )
+}
+
+console.log(reverseNumber(23534895));
 
 /**
  * TODO destruct the following object to get name and age.
@@ -32,11 +53,17 @@ const person = {
   },
 };
 
+const {name, info: {age}} = person;
+console.log(name);
+console.log(age);
+
+
 /**
  * TODO destruct the following array the first and second element, and the remaining elements in a third variable.
  */
 
 const thisArray = ["apple", "banana", "cherry", "dates", "elderberry", "fig"];
+const [first, second, ...third] = thisArray;
 
 /**
  * !!!!OPTIONAL!!!!
@@ -50,4 +77,31 @@ const thisArray = ["apple", "banana", "cherry", "dates", "elderberry", "fig"];
  *    isPalindrome("coding") should return false
  */
 
-function isPalindrome() {}
+function isPalindrome(check) {
+  let transform = check.toString()
+  if ( transform[0] === transform[transform.length - 1] ){
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(isPalindrome("ttet"));
+console.log(isPalindrome("tteh"));
+console.log(isPalindrome(1233));
+console.log(isPalindrome(3313));
+
+//refactory
+function isPalindrome1(check1) {
+  const transform1 = check1.toString().toLowerCase().replace(/\s+/g, "");
+
+  for (let i = 0; i < transform1.length / 2; i++) {
+    if (transform1[i] !== transform1[transform1.length - 1 - i]) {
+      return false
+    }
+  }
+  return true
+}
+console.log(isPalindrome1("madam"));
+console.log(isPalindrome1("tteh"));
+console.log(isPalindrome1(12321));
+console.log(isPalindrome1("race car"));
