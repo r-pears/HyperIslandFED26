e7d7fffb2f4ba2bcab5dd3ac2f7db7be1821cc79
goksinb <gbasok@gmail.com>
2025-01-14 09:51:42 +0100

    assignment 6 done

--- 6/script.js
+++ 6/script.js
@@ -1,23 +1,43 @@
+//quizappgoksinb.netlify.app
+
 // TODO: Set item in localStorage
+https: localStorage.setItem("key", "value");
 
 // TODO: Get item from localStorage
+const value = localStorage.getItem("key");
+console.log(value);
 
 // TODO: Remove item from localStorage
+localStorage.removeItem("key");
 
 // TODO: Clear all items from localStorage
+localStorage.clear();
 
 // TODO: Add an object to localStorage
+const obj = {Car: "Audi", Model: "A5"};
+localStorage.setItem("user", JSON.stringify(obj));
 
 // TODO: Get an object from localStorage
+const user = JSON.parse(localStorage.getItem("user"));
+console.log(user);
 
 // TODO: Set item in sessionStorage
+sessionStorage.setItem("key", "value");
 
 // TODO: Get item from sessionStorage
+const value = sessionStorage.getItem("key");
+console.log(value);
 
 // TODO: Remove item from sessionStorage
+sessionStorage.removeItem("key");
 
 // TODO: Clear all items from sessionStorage
+sessionStorage.clear();
 
 // TODO: Add an object to sessionStorage
+const obj = {name: "Adam", age: 27};
+sessionStorage.setItem("user", JSON.stringify(obj));
 
 // TODO: Get an object from sessionStorage
+const user = JSON.parse(sessionStorage.getItem("user"));
+console.log(user);
