73cfb375942462652f648c3e4e2c1a910effc949
odai-dh <odai.dahi@hyperisland.se>
2025-01-14 16:32:55 +0100

    a7

--- 7/script.js
+++ 7/script.js
@@ -7,8 +7,15 @@
  *   reverseString("world") should return "dlrow"
  *   reverseString("12345") should return "54321"
  */
-function reverseString() {}
+function reverseString(str) {
+let revers = "";
+for(let i = str.length-1; i >= 0; i--){
+  revers += str[i];
+}
+return revers;
+}
 
+console.log(reverseString("Hello"))
 /**
  * TODO write a JavaScript function that takes a number and returns it in the reverse order.
  * Should return the reversed number.
@@ -18,8 +25,25 @@ function reverseString() {}
  *   reverseNumber(101010) should return 10101
  */
 
-function reverseNumber() {}
-
+function reverseNumber(number) {
+let revNumber = 0
+while(number > 0){
+  revNumber = revNumber * 10 + number % 10;
+  number = Math.floor(number / 10);
+}
+return revNumber;
+}
+console.log(reverseNumber(12345))
+//OR
+function reverseFunction(num) {
+  let numStr = num.toString();
+  let revStr = '';
+  for (let i = numStr.length - 1; i >= 0; i--) {
+      revStr += numStr[i];
+  }
+  return parseInt(revStr);
+};
+console.log(reverseFunction(12345))
 /**
  * TODO destruct the following object to get name and age.
  */
@@ -31,6 +55,9 @@ const person = {
     job: "Developer",
   },
 };
+const name = person.name;
+const age = person.info.age;
+console.log(name, age)
 
 /**
  * TODO destruct the following array the first and second element, and the remaining elements in a third variable.
@@ -38,6 +65,10 @@ const person = {
 
 const thisArray = ["apple", "banana", "cherry", "dates", "elderberry", "fig"];
 
+const [first, second, ...rest] = thisArray;
+console.log(first)
+console.log(second)
+console.log(rest)
 /**
  * !!!!OPTIONAL!!!!
  * TODO Write a JavaScript function that checks if a string is a palindrome. A palindrome is a word, phrase, number, or other sequence of characters that reads * the same forward and backward. For example, "madam" is a palindrome.
@@ -50,4 +81,14 @@ const thisArray = ["apple", "banana", "cherry", "dates", "elderberry", "fig"];
  *    isPalindrome("coding") should return false
  */
 
-function isPalindrome() {}
+function isPalindrome(str) {
+  const fixedStr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+  const revStr = fixedStr.split('').reverse().join('');
+  return fixedStr === revStr;
+}
+
+console.log(isPalindrome("madam")); // true
+console.log(isPalindrome("hello")); // false
+console.log(isPalindrome("12321")); // true
+console.log(isPalindrome("race car")); // true
+console.log(isPalindrome("coding")); // false
