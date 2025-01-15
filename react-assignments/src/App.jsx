b73718d33246b7f10c69f073dc97a12e30551f4b
odai-dh <odai.dahi@hyperisland.se>
2025-01-15 16:18:33 +0100

    assignment solution

---
+++ react-assignments/src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import Component1 from "./Component/Component1/Component1";
+
+function App() {
+  const person = {
+    name: "John",
+    info: {
+      age: 25,
+      city: "New York",
+      job: "Developer",
+      hobbies: ["reading", "coding", "gaming"],
+    },
+  };
+
+  return (
+    <div>
+        <h1>Person Info</h1>
+        <Component1 person={person} />
+      {/* TODO create a child component in proper jsx, the component takes the person object as props and render the data */}
+    </div>
+  );
+}
+
+export default App;
