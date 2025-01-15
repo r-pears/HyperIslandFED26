3177834354f09b9ded02245ab3c884222dcdb343
Asher-zx <zhixuan.wang@hyperisland.se>
2025-01-15 22:08:42 +0100

    initial

---
+++ react-assignments/src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+import "./component/PersonInfo.css"
+import PersonInfo from "./component/PersonInfo";
+
+function App() {
+  const person = {
+    name: "John",
+    info: {
+      age: 25,
+      city: "New York",
+      job: "Developer",
+      hobbies: ["reading", "coding", "gaming"],
+    },
+  };
+
+  return (
+    <div>
+      <header>
+        <h1>This is React</h1>
+          <PersonInfo person={props}/>
+      </header>
+      {/* TODO create a child component in proper jsx, the component takes the person object as props and render the data */}
+    </div>
+  );
+}
+
+export default App;
---
+++ react-assignments/src/component/PersonInfo.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function PersonInfo({props}) {
+    return (
+        <div class="container">
+            <h1>Name: {props.name}</h1>
+            <h2>Age: {props.info.age}</h2>
+            <h2>City: {props.info.city}</h2>
+            <h2>Job: {props.info.job}</h2>
+            <h2>Hobbies: </h2>
+            <ul>
+                {props.info.hobbies.map((hobby) => (
+                <li key={hobby}>{hobby}</li>
+                ))}
+            </ul>   
+        </div>
+    )
+}
+
+export default PersonInfo;
