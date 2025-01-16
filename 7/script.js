e95768916b2062c31f6c020fa16d63d995389ce7
Sigstus985 <siggeluren@gmail.com>
2025-01-16 14:37:25 +0100

    assignment 7 done

--- 7/script.js
+++ 7/script.js
@@ -7,7 +7,17 @@
  *   reverseString("world") should return "dlrow"
  *   reverseString("12345") should return "54321"
  */
-function reverseString() {}
+function reverseString(string) {
+	let newstring = "";
+	for (let i = 1; i < string.length + 1; i++) {
+		newstring += string[string.length - i];
+	}
+	return newstring;
+}
+
+console.log(reverseString("hello"));
+console.log(reverseString("world"));
+console.log(reverseString("12345"));
 
 /**
  * TODO write a JavaScript function that takes a number and returns it in the reverse order.
@@ -18,25 +28,42 @@ function reverseString() {}
  *   reverseNumber(101010) should return 10101
  */
 
-function reverseNumber() {}
+function reverseNumber(number) {
+	let stringedNumber = number.toString();
+	let newNumber = "";
+	for (let i = 1; i < stringedNumber.length + 1; i++) {
+		newNumber += stringedNumber[stringedNumber.length - i];
+	}
+	return Number(newNumber);
+}
+
+console.log(reverseNumber(12345));
+console.log(reverseNumber(98765));
+console.log(reverseNumber(101010));
 
 /**
  * TODO destruct the following object to get name and age.
  */
 const person = {
-  name: "John",
-  info: {
-    age: 30,
-    city: "New York",
-    job: "Developer",
-  },
+	name: "John",
+	info: {
+		age: 30,
+		city: "New York",
+		job: "Developer",
+	},
 };
 
+const {name, info:{age}} = person
+
+console.log(name, age)
+
 /**
- * TODO destruct the following array the first and second element, and the remaining elements in a third variable.
+ * TODO destruct the following array the first and second element, and the remaining elements in a third string.
  */
 
 const thisArray = ["apple", "banana", "cherry", "dates", "elderberry", "fig"];
+
+const [apple, banana, ...rest] = thisArray
 
 /**
  * !!!!OPTIONAL!!!!
@@ -49,5 +76,3 @@ const thisArray = ["apple", "banana", "cherry", "dates", "elderberry", "fig"];
  *    isPalindrome("race car") should return true
  *    isPalindrome("coding") should return false
  */
-
-function isPalindrome() {}
