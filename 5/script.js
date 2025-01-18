76d86a32d456abca99d8365914499655da60864c
Xutik <kseniia.prakhova@hyperisland.se>
2025-01-18 22:34:11 +0100

    5 Ass

--- 5/script.js
+++ 5/script.js
@@ -6,21 +6,41 @@ const apiUrl = "https://api.example.com/data";
 function fetchData(url) {
   fetch(url)
     .then((response) => {
-      if (response.ok) {
+      if (!response.ok) {
         throw new Error("Network response was not ok " + response.statusText);
       }
       return response.json();
     })
     .then((data) => {
       console.log(data);
-    });
+    })
+    .catch((e) =>{
+      console.log(e)
+    });    
 }
 
 fetchData(apiUrl);
 
 // Function to make a POST request
 // TODO: Implement the function
-function postData(url, data) {}
+function postData(url, data) {
+  fetch(url, {
+    method: "POST",
+    body: JSON.stringify(data),
+  })
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("Network response was not ok " + response.statusText);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((e) =>{
+    console.log(e)
+  }); 
+}
 
 const dataToPost = {
   name: "John Doe",
