b45f1e97e57d5816530e96b8aedeaf203523784a
ffups <142579057+ffups@users.noreply.github.com>
2025-01-20 15:57:28 +0100

    tio klårt

--- react-assignments/src/App.jsx
+++ react-assignments/src/App.jsx
@@ -25,10 +25,3 @@ function App() {
 }
 
 export default App;
-
-// Create a Component, that does an API call to get characters from Game of Thrones and display them in a list. https://thronesapi.com/api/v2/Characters
-
-// use the Fetch API to get the data
-// use the useEffect hook to fetch the data when the component mounts
-// use the useState hook to store the data
-// display the data in a list
