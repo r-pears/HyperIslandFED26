1637978316b7586a4fd833173ecc3d0a6ad706a6
swathismenon <swathi.menon@hyperisland.se>
2025-01-20 02:26:18 +0100

    Implement form and add state handling in MyStateComponent

--- react-assignments/src/App.jsx
+++ react-assignments/src/App.jsx
@@ -17,8 +17,8 @@ function App() {
       <header>
         <h1>This is React</h1>
       </header>
-      {/* TODO create a child component in proper jsx, the component takes the person object as props and render the data */}
-      <PropsComponent person={person} />
+       {/* TODO create a child component in proper jsx, the component takes the person object as props and render the data */}
+       <PropsComponent person={person} />
     </div>
   );
 }
--- react-assignments/src/Components/MyStateComponent/MyStateComponent.jsx
+++ react-assignments/src/Components/MyStateComponent/MyStateComponent.jsx
@@ -1,12 +1,43 @@
 import React, { useState } from "react";
 
 const MyStateComponent = () => {
-  // TODO create a form using useState hook, the form should have two input fields, one for name and one for age, and a submit button and handle form data. Submit/console.log the form data.
+
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+
+ 
+  const handleSubmit = (event) => {
+    event.preventDefault(); 
+    console.log("Form Submitted: ", { name, age });
+  };
+
   return (
     <div>
       <h1>My State Component</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            value={name}
+            onChange={(e) => setName(e.target.value)} 
+          />
+        </div>
+        <div>
+          <label htmlFor="age">Age:</label>
+          <input
+            type="number"
+            id="age"
+            value={age}
+            onChange={(e) => setAge(e.target.value)} 
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 };
 
 export default MyStateComponent;
+
