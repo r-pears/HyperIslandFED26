e1942d621946a9cf58222df4c529fa4b50963ce2
ffups <142579057+ffups@users.noreply.github.com>
2025-01-20 11:48:05 +0100

    7 done

--- 7/script.js
+++ 7/script.js
@@ -1,5 +1,5 @@
 /**
- * TODO write a JavaScript function that takes a string and returns it in revserse order.
+ * TODO write a JavaScript function that takes a string and returns it in reverse order.
  * DO NOT use the built-in reverse() method.
  * Should return the reversed string.
  * For example:
@@ -7,7 +7,13 @@
  *   reverseString("world") should return "dlrow"
  *   reverseString("12345") should return "54321"
  */
-function reverseString() {}
+function reverseString(str) {
+  let reversed = '';
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+}
 
 /**
  * TODO write a JavaScript function that takes a number and returns it in the reverse order.
@@ -17,8 +23,10 @@ function reverseString() {}
  *   reverseNumber(98765) should return 56789
  *   reverseNumber(101010) should return 10101
  */
-
-function reverseNumber() {}
+function reverseNumber(num) {
+  const reversedStr = num.toString().split('').reverse().join('');
+  return parseInt(reversedStr, 10);
+}
 
 /**
  * TODO destruct the following object to get name and age.
@@ -32,11 +40,14 @@ const person = {
   },
 };
 
+const { name, info: { age } } = person;
+
 /**
  * TODO destruct the following array the first and second element, and the remaining elements in a third variable.
  */
-
 const thisArray = ["apple", "banana", "cherry", "dates", "elderberry", "fig"];
+
+const [first, second, ...rest] = thisArray;
 
 /**
  * !!!!OPTIONAL!!!!
@@ -49,5 +60,8 @@ const thisArray = ["apple", "banana", "cherry", "dates", "elderberry", "fig"];
  *    isPalindrome("race car") should return true
  *    isPalindrome("coding") should return false
  */
-
-function isPalindrome() {}
+function isPalindrome(str) {
+  const cleanedStr = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+  const reversedStr = cleanedStr.split('').reverse().join('');
+  return cleanedStr === reversedStr;
+}
