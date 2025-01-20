b517c3a3be5157925f416f55cd3481dd512aea83
Sigstus985 <siggeluren@gmail.com>
2025-01-20 13:31:30 +0100

    Assignment 9 (i think?) turned in

--- react-assignments/src/App.jsx
+++ react-assignments/src/App.jsx
@@ -3,29 +3,25 @@ import ChildComponent from "./ChildComponent";
 import MyStateComponent from "./Components/MyStateComponent/MyStateComponent";
 
 function App() {
-  const person = {
-    name: "John",
-    info: {
-      age: 25,
-      city: "New York",
-      job: "Developer",
-      hobbies: ["reading", "coding", "gaming"],
-    },
-  };
+	const person = {
+		name: "John",
+		info: {
+			age: 25,
+			city: "New York",
+			job: "Developer",
+			hobbies: ["reading", "coding", "gaming"],
+		},
+	};
 
-  return (
-    <div>
-      <header>
-        <h1>This is React</h1>
-      </header>
-      {
-        ChildComponent( person)
-      }
-      {
-        MyStateComponent()
-      }
-    </div>
-  );
+	return (
+		<div>
+			<header>
+				<h1>This is React</h1>
+			</header>
+			{ChildComponent(person)}
+			{MyStateComponent()}
+		</div>
+	);
 }
 
 export default App;
--- react-assignments/src/Components/MyStateComponent/MyStateComponent.jsx
+++ react-assignments/src/Components/MyStateComponent/MyStateComponent.jsx
@@ -2,16 +2,38 @@ import React, { useState } from "react";
 
 const MyStateComponent = () => {
 	// TODO create a form using useState hook, the form should have two input fields, one for name and one for age, and a submit button and handle form data. Submit/console.log the form data.
-	const [name, setName] = useState("John");
-	const [age, setAge] = useState(10);
+	const [formData, setFormData] = useState({ name: "", age: "" });
 
+	function handleChange(e) {
+		const { name, value } = e.target;
+		setFormData({ ...formData, [name]: value });
+	}
+
+	function handleSubmit(e) {
+		e.preventDefault();
+		console.log("Form Data:", formData);
+	}
 
 	return (
 		<div>
+			<br />
 			<h1>My State Component</h1>
-			<form action="">
-				<input type="text" placeholder="Name" onChange={setName} />
-				<input type="number" placeholder="Age" onChange={setAge} />
+			<form onSubmit={handleSubmit}>
+				<input
+					type="text"
+					placeholder="Name"
+					name="name"
+					value={formData.name}
+					onChange={handleChange}
+				/>
+				<input
+					type="number"
+					placeholder="Age"
+					name="age"
+					value={formData.age}
+					onChange={handleChange}
+				/>
+				<button type="submit">Submit</button>
 			</form>
 		</div>
 	);
