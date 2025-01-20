1cb6ac45531519655bfafcc1eecae94f0b85e927
Dylano <noob.hyper@gmail.com>
2025-01-20 17:13:33 +0100

    jesus christ

--- react-assignments/src/App.jsx
+++ react-assignments/src/App.jsx
@@ -1,12 +1,24 @@
 import "./App.css";
-import MyStateComponent from "./Components/MyStateComponent/MyStateComponent";
+import PropsComponent from "./Components/MyStateComponent/PropsComponent";
 
 function App() {
   
+  const person = {
+    name: "John",
+    info: {
+      age: 25,
+      city: "New York",
+      job: "Developer",
+      hobbies: ["reading", "coding", "gaming"],
+    },
+  };
 
   return (
     <div>
-      <MyStateComponent />
+      <header>
+        <h1>This is React</h1>
+      </header>
+      {/* TODO create a child component in proper jsx, the component takes the person object as props and render the data */}
     </div>
   );
 }
