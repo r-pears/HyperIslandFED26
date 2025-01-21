8354bd6b2c2faa8225a05529182b4f572dc3ce95
Fatima <174022648+TehFat@users.noreply.github.com>
2025-01-21 19:19:42 +0100

    done asssignment 10

--- react-assignments/src/App.jsx
+++ react-assignments/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import PropsComponent from "./Components/MyStateComponent/PropsComponent";
+import EffectsComponent from "./Components/Effect/EffectsComponent";
 
 function App() {
   const person = {
@@ -19,7 +20,11 @@ function App() {
       </header>
       {/* TODO create a child component in proper jsx, the component takes the person object as props and render the data */}
       <PropsComponent person={person} />
+      <>
+      <EffectsComponent/>
+    </>
     </div>
+    
   );
 }
 
--- react-assignments/src/Components/Effect/EffectsComponent.jsx
+++ react-assignments/src/Components/Effect/EffectsComponent.jsx
@@ -3,3 +3,33 @@
 // - use the useEffect hook to fetch the data when the component mounts
 // - use the useState hook to store the data
 // - display the data in a list
+import React,  { useState, useEffect } from 'react'
+
+export default function EffectsComponent() {
+    const [data, setData] = useState([]);
+    const [Loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+  
+    useEffect(() => {
+      fetch("https://thronesapi.com/api/v2/Characters")
+        .then((response) => response.json())
+        .then((data) => setData(data))
+        .catch((error) => setError(error.message))
+        .finally(() => setLoading(false));
+    }, []); 
+  
+    if (Loading) return <h1>Loading...</h1>;
+
+    if (error) return <h1>Error: {error}</h1>; // Show error if fetch fails
+  
+    return (
+      <>
+        <h1>Game of Throne Characters</h1>
+        <ul>
+          {data?.map((object) => (
+            <li key={object.id}>{object.fullName}</li>
+          ))}
+        </ul>
+      </>
+    );
+  }
