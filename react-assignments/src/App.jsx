fcfd1d1900a950208062ee34835e2c65cc6f6217
Alexandra <alexandra.riva@hyperisland.se>
2025-01-21 22:35:28 +0100

    10 Done

--- react-assignments/src/App.jsx
+++ react-assignments/src/App.jsx
@@ -1,24 +1,10 @@
 import "./App.css";
-import PropsComponent from "./Components/MyStateComponent/PropsComponent";
+import GameOfThronesCharacters from "./Components/Effect/EffectsComponent";
 
 function App() {
-  const person = {
-    name: "John",
-    info: {
-      age: 25,
-      city: "New York",
-      job: "Developer",
-      hobbies: ["reading", "coding", "gaming"],
-    },
-  };
-
   return (
     <div>
-      <header>
-        <h1>This is React</h1>
-      </header>
-      {/* TODO create a child component in proper jsx, the component takes the person object as props and render the data */}
-      <PropsComponent person={person} />
+      <GameOfThronesCharacters />
     </div>
   );
 }
